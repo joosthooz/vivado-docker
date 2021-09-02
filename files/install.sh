bbdc4b7143c36dd282a2a07e52ba9da1deeb8e3b
Joost Hoozemans <jjhoozemans@DESKTOP-9SRO993.localdomain>
2021-09-02 14:09:44 +0200

    Some initial changes to install Vivado with a web installer

--- files/install.sh
+++ files/install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-/opt/Xilinx_Vivado_2019.2_1106_2127/xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config /opt/install_config.txt
+echo "Extracting Vivado Installer..."
+cd /opt/ && tar -zf Xilinx_Vivado_Vitis_Update_2019.2.1_1205_0436.tar.gz && chmod +x /opt/Xilinx_Vivado_Vitis_Update_2019.2.1_1205_0436/xsetup
+echo "Running Vivado Installer in Batch mode..."
+/opt/Xilinx_Vivado_Vitis_Update_2019.2.1_1205_0436/xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config /opt/install_config.txt
